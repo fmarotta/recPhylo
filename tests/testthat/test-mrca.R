172b66195e261c8b70852d726b9bcf84c4cc3f03
Federico Marotta <federico.marotta@embl.de>
2024-06-26 11:53:20 +0200

    fix tests

--- tests/testthat/test-mrca.R
+++ tests/testthat/test-mrca.R
@@ -1,5 +1,5 @@
 test_that("the mrca() function works", {
-  ex <- RecPhylo$new(recphylo_example("example_1.recphyloxml"))
+  ex <- RecPhylo$new(recphylo_example("example_1.recphyloxml"), use_branch_length = 10)
   expect_equal(mrca(ex$spList, c("A")), "A")
   expect_equal(mrca(ex$spList, c("D", "C")), "CD")
   expect_equal(mrca(ex$spList, c("D", "B")), "BCD")
